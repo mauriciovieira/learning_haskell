2342b2fa502a697c07fa0e1ffd8b77f7da61f35b
Mauricio Vieira <mauricio@mauriciovieira.net>
2014-11-16 23:29:07 -0200

    Classes and exercise 13

--- FP101x/ListComprehension.hs
+++ FP101x/ListComprehension.hs
@@ -6,6 +6,22 @@ sorted xs =
    and [x <= y | (x,y) <- pairs xs]
 
 positions :: Eq a => a -> [a] -> [Int]
-positions x xs = 
+positions x xs =
    [i | (x', i) <- zip xs [0..n], x == x']
    where n = length xs - 1
+
+positions' :: Eq a => a -> [a] -> [Int]
+positions' x xs =
+   [i | (x', i) <- zip xs [0..], x == x']
+
+{-
+lowers :: String -> Int
+lowers xs =
+   length [x | x <- xs, isLower x]
+-}
+
+divides :: Int -> Int -> Bool
+divides x y = x `mod` y == 0
+
+divisors :: Int -> [Int]
+divisors x = [ d | d <- [1..x], x `divides` d]
