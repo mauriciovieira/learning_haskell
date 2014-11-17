9ddd4ffc7c6c7ec9bd791261a9a897fd17f4a12d
Mauricio Vieira <mauricio@mauriciovieira.net>
2014-11-16 23:15:29 -0200

    List Comprehensions class

---
+++ FP101x/ListComprehension.hs
@@ -0,0 +1,11 @@
+pairs :: [a] -> [(a,a)]
+pairs xs = zip xs (tail xs)
+
+sorted :: Ord a => [a] -> Bool
+sorted xs =
+   and [x <= y | (x,y) <- pairs xs]
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = 
+   [i | (x', i) <- zip xs [0..n], x == x']
+   where n = length xs - 1
