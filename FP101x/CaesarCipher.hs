66264e657d3b8fb2d631c2f16b5276ddc766c701
Mauricio Vieira <mauricio.vieira@dafiti.com.br>
2014-11-17 16:34:33 -0200

    Week 4 - Exercise 7

--- FP101x/CaesarCipher.hs
+++ FP101x/CaesarCipher.hs
@@ -6,9 +6,23 @@ let2int c = ord c - ord 'a'
 int2let :: Int -> Char
 int2let n = chr (ord 'a' + n)
 
+let2int' :: Char -> Int
+let2int' c = ord c - ord 'A'
+
+int2let' :: Int -> Char
+int2let' n = chr (ord 'A' + n)
+
 shift :: Int -> Char -> Char
-shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
+shift n c | isLower c = int2let ((let2int c+n) `mod` 26)
           | otherwise = c
+
+shift' :: Int -> Char -> Char
+shift' n c | isLower c = int2let ((let2int c + n) `mod` 26)
+           | isUpper c = int2let' ((let2int' c + n) `mod` 26)
+           | otherwise = c
 
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
+
+encode' :: Int -> String -> String
+encode' n xs = [shift' n x | x <- xs]
