d09690dad6c4518ff8b8ddc12355d0708a76ee8d
Mauricio Vieira <mauricio@mauriciovieira.net>
2014-11-16 23:56:53 -0200

    Exercises 3 and 12

--- FP101x/ListComprehension.hs
+++ FP101x/ListComprehension.hs
@@ -25,3 +25,33 @@ divides x y = x `mod` y == 0
 
 divisors :: Int -> [Int]
 divisors x = [ d | d <- [1..x], x `divides` d]
+
+
+riffleA :: [a] -> [a] -> [a]
+riffleA xs ys = concat [ [x,y] | x <- xs, y <- ys ]
+
+riffleB :: [a] -> [a] -> [a]
+riffleB xs ys = concat [ [x,y] | (x, y) <- xs `zip` ys ]
+
+{- parse error on input ‘|’
+riffleC :: [a] -> [a] -> [a]
+riffleC xs ys = [ x, y | (x, y)  <- xs `zip` ys ]
+-}
+
+{-Couldn't match expected type ‘a’ with actual type ‘[a]’
+riffleD :: [a] -> [a] -> [a]
+riffleD xs ys = [ x : [y] | x <- xs, y <- ys ]
+-}
+
+pythsA :: Int -> [(Int, Int, Int)]
+pythsA n = [(x,y,z) | x <- [1..n], y <- [1..x], z <- [1..y], x^2 + y^2 == z^2]
+
+pythsB :: Int -> [(Int, Int, Int)]
+pythsB n = [(x,y,z) | x <- [1..n], y <- [x..n], z <- [y..n], x^2 + y^2 == z^2]
+
+pythsC :: Int -> [(Int, Int, Int)]
+pythsC n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+
+pythsD :: Int -> [(Int, Int, Int)]
+pythsD n = [(x,y,z) | x <- [1..n], y <- [1..x], z <- [1..y]]
+
