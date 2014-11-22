ec9b12e78ab0c75d545cd35e5fa4c84409670a45
Mauricio Vieira <mauricio@mauriciovieira.net>
2014-11-22 16:07:38 -0200

    Quicksort implementation

--- FP101x/Ordering.hs
+++ FP101x/Ordering.hs
@@ -2,3 +2,9 @@ isMonotone :: Ord a => [a] -> Bool
 isMonotone [] = True
 isMonotone xs = and [x <= y | (x, y) <- zip xs (tail xs)]
 
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+               where
+                 smaller = [ a | a <-xs, a <= x ]
+                 larger = [ b | b <- xs, b > x ]
