d8f2ef4236854555048fcf465ba5b43148f947f3
Mauricio Vieira <mauricio.vieira@dafiti.com.br>
2014-11-26 18:38:24 -0200

    Beginning to solve Lesson 6

---
+++ FP101x/Lesson6Exercises.hs
@@ -0,0 +1,23 @@
+allA :: (a -> Bool) -> [a] -> Bool
+allA = undefined
+
+allB :: (a -> Bool) -> [a] -> Bool    
+allB = undefined
+
+allC :: (a -> Bool) -> [a] -> Bool    
+allC = undefined
+
+allD :: (a -> Bool) -> [a] -> Bool    
+allD = undefined
+
+allE :: (a -> Bool) -> [a] -> Bool    
+allE = undefined
+
+allF :: (a -> Bool) -> [a] -> Bool    
+allF = undefined
+
+allG :: (a -> Bool) -> [a] -> Bool    
+allG = undefined
+
+allH :: (a -> Bool) -> [a] -> Bool    
+allH = undefined
