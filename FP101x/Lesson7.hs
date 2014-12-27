487ec891cb21d1ae5fe7b31826ff583d71c1b72c
Mauricio Vieira <mauricio@mauriciovieira.net>
2014-12-27 00:03:51 -0200

    Lesson7 - writing parser

--- FP101x/Lesson7.hs
+++ FP101x/Lesson7.hs
@@ -1,4 +1,22 @@
 module Lesson7 where
 
-import ProgrammingInHaskell.Parsing
+type Parser a = String -> [(a, String)]
 
+item :: Parser Char
+item = \ inp -> case inp of
+                 []     -> []
+                 (x:xs) -> [(x,xs)]
+
+failure :: Parser a
+failure = \ inp -> []
+
+return :: a -> Parser a
+return v = \ inp -> [(v, inp)]
+
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = \ inp -> case p inp of
+                    []        -> parse q inp
+                    [(v,out)] -> [(v,out)]
+
+parse :: Parser a -> String -> [(a,String)]
+parse p inp = p inp
