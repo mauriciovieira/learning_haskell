3137b10500186b50dc39303481ab0bd8c5b8ec4e
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-11-29 21:22:04 -0200

    More list comprehensions

--- ForGreaterGood/2-starting-out/baby.hs
+++ ForGreaterGood/2-starting-out/baby.hs
@@ -11,3 +11,9 @@ doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
