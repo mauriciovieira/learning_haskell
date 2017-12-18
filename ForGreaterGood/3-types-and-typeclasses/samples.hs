a7fb9d4ad8245e66395de07d13658ba8827fe3b8
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-12-17 23:00:31 -0200

    Add factorial function

--- ForGreaterGood/3-types-and-typeclasses/samples.hs
+++ ForGreaterGood/3-types-and-typeclasses/samples.hs
@@ -3,3 +3,6 @@ removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
